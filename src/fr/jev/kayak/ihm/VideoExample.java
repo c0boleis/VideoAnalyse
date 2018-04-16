9ad409a6e02737036fb55e30025a1d6e3d1a6438
cb <corentinboleis@hotmail.fr>
2018-04-16 20:41:07 +0200

    ajustement pour windows

--- src/fr/jev/kayak/ihm/VideoExample.java
+++ src/fr/jev/kayak/ihm/VideoExample.java
@@ -16,6 +16,7 @@ import uk.co.caprica.vlcj.binding.LibVlc;
 import uk.co.caprica.vlcj.player.MediaPlayerFactory;
 import uk.co.caprica.vlcj.player.embedded.EmbeddedMediaPlayer;
 import uk.co.caprica.vlcj.player.embedded.FullScreenStrategy;
+import uk.co.caprica.vlcj.player.embedded.windows.Win32FullScreenStrategy;
 import uk.co.caprica.vlcj.player.embedded.x.XFullScreenStrategy;
 import uk.co.caprica.vlcj.runtime.RuntimeUtil;
 
@@ -23,8 +24,20 @@ import uk.co.caprica.vlcj.runtime.RuntimeUtil;
 public class VideoExample {
 	
 //	private MediaP
+	
+	public static final String WINDOWS_OS = "windows";
 
 	public static void main(String[] args) {
+		String os = System.getProperty("os.name");
+		System.out.println("OS: "+os);
+		boolean isWindowsOS = false;
+		if(os != null) {
+			os = os.trim().toLowerCase();
+			if(os.contains(WINDOWS_OS)) {
+				isWindowsOS = true;
+				System.out.println("Config for windows.");
+			}
+		}
 		JFrame f = new JFrame();
 		f.setLocation(100, 100);
 		f.setSize(500, 500);
@@ -39,14 +52,21 @@ public class VideoExample {
 		f.add(p);
 		f.setVisible(true);
 	
-//		NativeLibrary.addSearchPath(RuntimeUtil.getLibVlcLibraryName(),"/usr/bin/vlc");
+		if(isWindowsOS) {
+			NativeLibrary.addSearchPath(RuntimeUtil.getLibVlcLibraryName(),"C:/Program Files (x86)/VideoLAN/VLC");
+		}
 		Native.loadLibrary(RuntimeUtil.getLibVlcLibraryName(),LibVlc.class);
-//		Native
 		MediaPlayerFactory mpf = new MediaPlayerFactory();
-		EmbeddedMediaPlayer emp = mpf.newEmbeddedMediaPlayer(new XFullScreenStrategy(f));
+		EmbeddedMediaPlayer emp = null;
+		if(isWindowsOS) {
+			emp = mpf.newEmbeddedMediaPlayer(new Win32FullScreenStrategy(f));
+		}else {
+			emp = mpf.newEmbeddedMediaPlayer(new XFullScreenStrategy(f));
+		}
+		
 		emp.setVideoSurface(mpf.newVideoSurface(c));
 //		emp.toggleFullScreen();
-		String file = "/home/corentin/Documents/video_test/RWFS8684.MP4";
+		String file = args[0];
 		
 		emp.prepareMedia(file);
 		emp.play();
